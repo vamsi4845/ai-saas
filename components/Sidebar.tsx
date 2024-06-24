42140f9a7a7331709df34fac0e0cbf19f0d58612
vamsisai <vamsisai.karumuri@gmail.com>
2024-06-24 23:54:29 +0530

    refractor:add routes in sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,6 +2,65 @@
 
 import Link from "next/link";
 import Image from "next/image";
+import { Montserrat } from "next/font/google";
+import { cn } from "@/lib/utils";
+import {
+  CodeIcon,
+  ImageIcon,
+  LayoutDashboard,
+  MessageSquare,
+  MusicIcon,
+  Settings,
+  VideoIcon,
+} from "lucide-react";
+
+const monsterrat = Montserrat({ weight: "600", subsets: ["latin"] });
+
+const routes = [
+  {
+    label: "Dashboard",
+    icon: LayoutDashboard,
+    href: "/dashboard",
+    color: "text-sky-500",
+  },
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    href: "/conversation",
+    color: "text-violet-500",
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    href: "/image",
+    color: "text-pink-700",
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    href: "/video",
+    color: "text-orange-700",
+  },
+  {
+    label: "Music Generation",
+    icon: MusicIcon,
+    href: "/music",
+    color: "text-emerald-700",
+  },
+  {
+    label: "Code Generation",
+    icon: CodeIcon,
+    href: "/code",
+    color: "text-green-700",
+  },
+  {
+    label: "Settings",
+    icon: Settings,
+    href: "/settings",
+    color: "text-black-700",
+  },
+];
+
 const Sidebar = () => {
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
@@ -10,7 +69,24 @@ const Sidebar = () => {
           <div className="relative w-8 h-8 mr-4">
             <Image fill alt="logo" src="/logo.png" />
           </div>
+          <h1 className={cn("text-2xl font-bold", monsterrat.className)}>
+            Ai-SaaS
+          </h1>
         </Link>
+        <div className="space-y-1">
+          {routes.map((route) => (
+            <Link
+              key={route.label}
+              href={route.href}
+              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg"
+            >
+              <div className="flex items-center flex-1">
+                <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                {route.label}
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
